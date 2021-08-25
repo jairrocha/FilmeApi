92df1a2412326d212d1e492d85d64f5f85571950
Jair <jairmsn@hotmail.com>
2021-08-25 20:01:57 -0300

    Enviando json

---
+++ FilmeAPI/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : Controller
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+
+        }
+    }
+}
