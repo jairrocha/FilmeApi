3aacbad906f90ce27db7df0ea6c76f2356ba7836
Jair <jairmsn@hotmail.com>
2021-08-28 13:45:10 -0300

    Add classe de contexto, string de conexçao e adicionando serviço

---
+++ FilmeAPI/Data/FilmeContext.cs
@@ -0,0 +1,20 @@
+﻿using FilmeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Data
+{
+    public class FilmeContext :DbContext
+    {
+        public FilmeContext(DbContextOptions <FilmeContext> options) : base(options)
+        {
+
+        }
+        public  DbSet<Filme> Filmes { get; set; }
+
+
+    }
+}
--- FilmeAPI/Startup.cs
+++ FilmeAPI/Startup.cs
@@ -1,7 +1,9 @@
+using FilmeAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,10 @@ namespace FilmeAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FilmeContext>
+                (opt => opt.UseMySQL(Configuration
+                .GetConnectionString("FilmeConnection"))
+                );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
