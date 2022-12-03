368185d2bf265d8cb1923704ff63772037e9c2db
Jair <jairmsn@hotmail.com>
2022-12-02 21:28:55 -0300

    refactor: classe startup

---
+++ FilmeAPI/Config/Configuration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using FilmeAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmeAPI.Config
+{
+    public static class Configuration
+    {
+        public static void Swagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmeAPI", Version = "v1" });
+            });
+        }
+        public static void ApiContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FilmeContext>
+                (opt => opt.UseSqlServer(configuration
+                .GetConnectionString("FilmeConnection")));
+        }
+        public static void Identity(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<IdentityContext>
+                (opt => opt.UseSqlServer(configuration
+                .GetConnectionString("IdentityConnection")));
+
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                 .AddEntityFrameworkStores<IdentityContext>()
+                 .AddDefaultTokenProviders();
+
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.AccessDeniedPath = "/Auth/forbidden";
+                options.Cookie.Name = "FilmeApi";
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.LoginPath = "/Auth/Unauthorized";
+                // ReturnUrlParameter requires 
+                //using Microsoft.AspNetCore.Authentication.Cookies;
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.SlidingExpiration = true;
+            });
+
+        }
+    }
+}
--- FilmeAPI/Startup.cs
+++ FilmeAPI/Startup.cs
@@ -1,3 +1,4 @@
+using FilmeAPI.Config;
 using FilmeAPI.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
@@ -14,61 +15,24 @@ namespace FilmeAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<FilmeContext>
-                (opt => opt.UseSqlServer(Configuration
-                .GetConnectionString("FilmeConnection"))
-                );
+            services.ApiContext(Configuration);
 
-
-            services.AddDbContext<IdentityContext>
-           (opt => opt.UseSqlServer(Configuration
-           .GetConnectionString("IdentityConnection"))
-           );
-
-
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<IdentityContext>()
-                .AddDefaultTokenProviders();
-
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Default Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.AllowedForNewUsers = true;
-                
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                //options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                options.Cookie.Name = "FilmeApi";
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-                //options.LoginPath = "/Identity/Account/Login";
-                // ReturnUrlParameter requires 
-                //using Microsoft.AspNetCore.Authentication.Cookies;
-                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
-                options.SlidingExpiration = true;
-            });
-
-
+            services.Identity(Configuration);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmeAPI", Version = "v1" });
-            });
+
+            services.Swagger();
+
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
@@ -79,17 +43,13 @@ namespace FilmeAPI
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmeAPI v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "FilmeAPI v1"));
             }
-
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
-            
             app.UseRouting();
-
             app.UseAuthorization();
-
 
             app.UseEndpoints(endpoints =>
             {
